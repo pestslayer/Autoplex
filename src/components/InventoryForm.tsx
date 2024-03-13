b7d62f77c48afac50fdf848f9b0a2f4e629a2ff3
Ryan <ryamcmorris@gmail.com>
2024-03-13 13:50:57 -0500

    Current Code

--- src/components/InventoryForm.tsx
+++ src/components/InventoryForm.tsx
@@ -65,3 +65,7 @@ const InvetoryForm = ( props:InvetoryFormProps) => {
 }
 
 export default InvetoryForm
+
+
+
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Carousel from '../components/Carousel'
 
 const home = () => {
